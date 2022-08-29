49ebb887a60c3cb9593ccd302f47bb8f69a1be20
jiya0617 <103304159+jiya0617@users.noreply.github.com>
2022-08-30 00:20:13 +0530

    Add files via upload

---
+++ complementBase10int.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int m=n;
+    int mask=0;
+    if(n==0){
+        return 1;
+    }
+    while(m>0){
+        m=m >> 1;
+       mask= (mask << 1)|1;
+    }
+    int ans=~(n) & mask;
+    cout<<ans;
+}
